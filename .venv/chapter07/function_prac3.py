27fdcbd2984f8f99592e1ae70fa89fe5174370fa
hyuniverse-dev <hyuniverse.dev@gmail.com>
2025-03-22 12:25:21 +0900

    function

--- .venv/chapter07/function_prac3.py
+++ .venv/chapter07/function_prac3.py
@@ -36,3 +36,57 @@
 		(테스트 케이스2)
 		최소 2개 이상의 숫자를 입력하세요.
 """
+
+
+# 함수 정의 - 여러 개의 정수를 제공하고, 최대값과 최소값을 구해서 반환시킨다.
+def find_max_min_value(numbers: list):
+    """
+    주어진 숫자 리스트에서 최대값과 최소값을 찾아서 반환하는 함수이다.
+
+    :param numbers: 숫자 리스트
+    :return: 최대값과 최소값
+    """
+
+    ##### 기본 방식
+    # max_value = numbers[0]  # 최대값을 담을 변수
+    # min_value = numbers[0]  # 최소값을 담을 변수
+
+    # 반복문을 사용해서 현재 가져온 요소가 max_value보다 크면 max_value 에 대입
+    # 반복문을 사용해서 현재 가져온 요소가 min_value보다 작으면 min_value 에 대입
+    # for number in numbers:
+    #     if number > max_value:
+    #         max_value = number
+    #
+    #     if number < min_value:
+    #         min_value = number
+
+    ##### max(), min() 함수 사용
+    max_value = max(numbers)
+    min_value = min(numbers)
+
+    # 결과값을 반환
+    return [max_value, min_value]
+
+
+# 사용자로부터 입력받을 받는 무한루프
+numbers = []
+# count = 0
+while True:
+    number = int(input("정수를 입력해주세요. (종료는 0을 입력) >>> "))
+
+    # 입력을 받은 데이터가 0이면 탈출
+    # 입력을 받은 데이터가 0이 아니면 리스트에 데이터 추가
+    # 입력받은 데이터가 총 2개 미만이면
+    if number != 0:
+        numbers.append(number)
+    else:
+        if len(numbers) < 2:
+            print("최소 2개 이상의 숫자를 입력하세요.")
+        else:
+            break
+
+# 최대/최소값 구하는 find_max_min_value() 호출해서 결과 반환
+res = find_max_min_value(numbers)
+
+# 결과를 반환받고 출력
+print(f"최대값: {res[0]}, 최소값: {res[1]}")
