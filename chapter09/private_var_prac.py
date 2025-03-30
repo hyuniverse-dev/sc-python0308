fcd6d786efdecc20f8d691b09de35064a4f3f973
hyuniverse-dev <hyuniverse.dev@gmail.com>
2025-03-30 09:37:26 +0900

    private_var_prac

---
+++ chapter09/private_var_prac.py
@@ -0,0 +1,20 @@
+'''
+파일명: private_var_prac.py
+'''
+
+# User 클래스 선언
+
+
+# 멤버 필드: user_id, user_password 프라이빗 변수로 선언
+#        : user_password 가 빈 문자열이면 "비밀번호는 빈문자열일 수 없습니다." 출력 user_password 에 user_id 를 대신 대입
+
+# user_password 게터와 세터
+#   : user_password 세터에는 빈 문자열인 경우 return
+
+################################################
+
+# User 객체 생성
+
+# user_password 게터를 사용해서 비밀번호 출력
+
+# user_password 세터를 사용해서 비밀번호 재할당(케이스1-빈문자열, 케이스2-새로운 비밀번호)
