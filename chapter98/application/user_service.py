5a9f62eb7c38fdb6358e0f6e2a0d022b43aa4bb5
hyuniverse-dev <hyuniverse.dev@gmail.com>
2025-04-12 12:55:23 +0900

    find

--- chapter98/application/user_service.py
+++ chapter98/application/user_service.py
@@ -5,13 +5,11 @@
 
 from chapter98.domain.user import User
 from chapter98.infra.user_repo import *
+from chapter99.auth import signin
 
 
 # 회원가입을 수행하는 함수
 def sign_up():
-    """
-    사용자의 정보를 입력받아 저장하는 함수
-    """
     try:
         id = input("아이디 >>> ")
         password = input("비밀번호 >>> ")
@@ -19,11 +17,27 @@ def sign_up():
         email = input("이메일 >>> ")
 
         user = User(id, password, name, email)
-        save(user) # 사용자 정보 저장
+        save(user)  # 사용자 정보 저장
     except ValueError as e:
         print(f"사용자 정보 저장 실패: {e}")
 
 
+# 로그인을 수행하는 함수
+def sign_in():
+    id = input("아이디 >>> ")
+    password = input("비밀번호 >>> ")
+
+    try:
+        user = find(id, password)
+        print(user)
+        if user:
+            print(f"회원이름: {user.name}")
+        else:
+            print("조회된 사용자가 없습니다.")
+    except ValueError as e:
+        print(e)
+
 
 if __name__ == "__main__":
-    sign_up()
+    # sign_up()
+    sign_in()
--- chapter98/infra/user_repo.py
+++ chapter98/infra/user_repo.py
@@ -5,17 +5,17 @@
 from chapter98.domain.user import User
 import json
 
+USER_FILE_PATH = "C:/sc-python0308-main/data/users.json"
+
 
 # 유저 정보를 저장하는 함수
 def save(user: User):
-    file_path = "C:/sc-python0308-main/data/users.json"
-
     # users.json에 동일한 id가 존재하는지 검사
     try:
-        users = load_data_fromjson(file_path=file_path)
+        users = load_data_fromjson(file_path=USER_FILE_PATH)
     except FileNotFoundError:
         users = []
-    except json.JSONDecodeError: # 정상 케이스는 아니지만 발생했기 때문에 추가로 처리
+    except json.JSONDecodeError:  # 정상 케이스는 아니지만 발생했기 때문에 추가로 처리
         users = []
 
     for item in users:
@@ -24,7 +24,21 @@ def save(user: User):
 
     users.append(user.__dict__)  # 객체의 메소드를 제외한 속성만을 딕셔너리로 반환
 
-    save_data_tojson(file_path=file_path, data=users)
+    save_data_tojson(file_path=USER_FILE_PATH, data=users)
+
+
+# 유저 정보를 조회하는 함수
+def find(id: str, password: str):
+    try:
+        users = load_data_fromjson(USER_FILE_PATH)
+    except FileNotFoundError:
+        raise ValueError(f"{id}은/는 존재하지 않습니다.")
+
+    for user in users:
+        if user.get("id") == id and user.get("password") == password:
+            return User(user.get("id"), user.get("password"), user.get("name"), user.get("email"))
+
+    # return User("unknown", "unknown1234", "unknown", "unknown@email.com") --- AttributeError 단순하게 처리하는 방법(1)
 
 
 # 공통 함수 -> JSON 파일에 데이터를 저장할 때 공통으로 호출해서 사용하는 함수
