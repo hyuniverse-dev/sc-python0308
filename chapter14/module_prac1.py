c15c5cc89109bf6d5ae8bf4e8a726b1569b62033
hyuniverse-dev <hyuniverse.dev@gmail.com>
2025-04-13 11:09:22 +0900

    random number

--- chapter14/module_prac1.py
+++ chapter14/module_prac1.py
@@ -1,19 +1,49 @@
 '''
 파일명: module_prac1.py
 
-다음은 로또 번호를 추첨하는 프로그램입니다. (?) 안을 채워서 프로그램을 완성하세요.
+다음은 로또 번호를 추첨하는 프로그램입니다. 프로그램을 완성하세요.
 '''
 
-###### random 모듈로 번호 6개 추첨하기 - 중복값 허용(1, 1, ..)
-
+###### (1) random 모듈로 번호 6개 추첨하기 - 중복값 허용(1, 1, ..)
 # random 모듈 추가
+import random
 
 # 번호를 담을 리스트인 numbers 초기화
+numbers = []
 
-# 루프 실행 (while문 또는 for문 선택)
+while len(numbers) < 6:  # 루프 실행 (while문 또는 for문 선택)
+    number = random.randint(1, 45)  # 난수 생성 - 1이상 45이하의 정수만 선택
+    numbers.append(number)  # 생성된 난수를 numbers 리스트에 담기
 
-    # 난수 생성 - 1이상 45이하의 정수만 선택
-    
-    # 생성된 난수를 numbers 리스트에 담기
-    
 # numbers 결과 출력
+print(numbers)
+
+###### (2) random 모듈로 번호 6개 추첨하기 - 중복값 미허용(1, 1, ..)
+
+numbers = []
+
+while len(numbers) < 6:
+    number = random.randint(1, 45)
+
+    # 중복값 미허용 조건문
+    if number not in numbers:
+        numbers.append(number)
+
+print(numbers)
+
+##### (3) random 모듈로 번호 6개 추첨하기 - 중복값 미허용(set 자료구조 사용)
+#         - set은 중복값을 허용하지 않는다.
+#         - 따라서 별도의 조건문이 필요하지 않다.
+numbers = set()
+
+while len(numbers) < 6:
+    number = random.randint(1, 45)
+    numbers.add(number)
+
+print(list(numbers))
+
+
+
+
+
+
